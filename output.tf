68876c766e14e45a45971e5b2776bb6ead0d9bf1
Manju Tamanampudi <manjukarri3@gmail.com>
2026-04-08 09:25:20 +0530

    one more commit

--- output.tf
+++ output.tf
@@ -1,3 +1,15 @@
 output "azs_info"{
     value = data.aws_availability_zones.available
 }
+output "vpc_id"{
+    value = aws_vpc.main.id
+}
+output "subnet_public"{
+    value = aws_subnet.public[*].id
+}
+output "subnet_private"{
+    value = aws_subnet.private[*].id
+}
+output "subnet_db"{
+    value = aws_subnet.db[*].id
+}
